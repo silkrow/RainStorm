83cb3c386823417d6d99be768f28beb1b043549a
silkrow <si1krow@outlook.com>
2024-12-09 12:58:13 -0600

    script

--- client/lk.sh
+++ client/lk.sh
@@ -3,6 +3,14 @@
 # Variables
 base_ip="fa24-cs425-68%02d.cs.illinois.edu"
 
+go_program_command="go run RainStorm.go app1_1 app1_2 10000.csv f2 3"
+# Launch Go program locally
+eval "$go_program_command &"  # Start the Go program in the background
+main_pid=$!
+echo "Go program started with PID $main_pid on $(hostname)"
+# Wait 1.5 seconds
+sleep 1.5
+
 # Choose two VMs to send the kill command
 target_vms=(02 06) # Example: send SIGINT to Go program on VM 02 and VM 06
 for vm in "${target_vms[@]}"; do
